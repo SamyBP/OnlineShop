420c6c0ddfcaf68df563bbc03fd5a6a17a25f4e9
Beni Pintea <b.pintea04@gmail.com>
2024-07-25 18:17:49 +0300

    indentation for procedures

--- procedures/AddToCart.sql
+++ procedures/AddToCart.sql
@@ -1,28 +1,31 @@
 CREATE OR REPLACE PROCEDURE os.add_to_cart(
 	IN integer,
 	IN integer,
-	IN integer)
+	IN integer
+)
 LANGUAGE 'plpgsql'
 AS $BODY$
+
 declare
-_cart_id int;
+    _cart_id int;
 begin
-select id into _cart_id from cart where user_id = $1 and is_active = true;
-if _cart_id is null then
-		insert into cart(user_id) values ($1) returning id into _cart_id;
-end if;
+    select id into _cart_id from cart where user_id = $1 and is_active = true;
+    if _cart_id is null then
+        insert into cart(user_id) values ($1) returning id into _cart_id;
+    end if;
 
-with cart_product_info as (
-    select
-        _cart_id as cart_id,
-        id as product_id,
-        $3 as quantity,
-        price
-    from
-        product
-    where id = $2
-)
-insert into cart_product(cart_id, product_id, quantity, price) select * from cart_product_info;
+    with cart_product_info as (
+        select
+            _cart_id as cart_id,
+            id as product_id,
+            $3 as quantity,
+            price
+        from
+            product
+        where id = $2
+    )
+    insert into cart_product(cart_id, product_id, quantity, price) select * from cart_product_info;
 
+    commit;
 end;
 $BODY$;
